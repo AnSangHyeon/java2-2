846f69979a54a5c0429296441159ca714cf7d2a5
AnSangHyeon <ash040906@naver.com>
2024-03-29 16:07:21 +0900

    [sh] 주석으로 설명 추가

--- src/App.java
+++ src/App.java
@@ -1,12 +1,10 @@
-package src;
-
 import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("월(1~12)을 입력하시오.");
+    System.out.print("월(1~12)을 입력하시오.");
 
     int month = sc.nextInt();
     if(month == 3 || month == 4 || month == 5) {
@@ -20,5 +18,6 @@ public class App {
     } else {
       System.out.println("잘못입력했습니다.");
     }
+    sc.close(); //Scanner사용하고 close안하면 오류는 안나지만 가비지컬렉션(메모리 낭비) 발생
   }
 }
